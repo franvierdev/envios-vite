572a4208da8236263f62c1753b6956aff6d01bb3
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2023-06-21 00:57:11 -0400

    asd

--- src/Prices.jsx
+++ src/Prices.jsx
@@ -40,7 +40,7 @@ export function Prices() {
 
           <button className=' text-green-900 font-bold text-2xl px-2 py-1 '>1 Dolar</button>
           <p className='text-3xl text-gray-100 text-shadow1'>=</p>
-          <input className=' py-1 text-shadow1 text-2xl  font-semibold text-yellow-400 bg-transparent border-gray-700 w-24' placeholder="Dolares"
+          <input className=' py-1 text-shadow1 text-2xl  font-semibold text-yellow-400 bg-transparent border-gray-700 w-24' placeholder="USD"
             onChange={(e) => setTitledolar(e.target.value)} value={titledolar} required autoFocus />
           <span className='text-shadow1 text-yellow-400 font-semibold text-2xl  ms-64 mt-1  absolute' >Bs</span>
         </ul>
@@ -50,7 +50,7 @@ export function Prices() {
 
           <button className=' text-green-900 text-2xl font-bold px-2 py-1 ' disabled={true} >1 Sol</button>
           <p className='text-3xl text-gray-100 text-shadow1'>=</p>
-          <input className=' px-3 py-1 text-shadow1 text-2xl bg-transparent font-semibold w-24 text-yellow-400' placeholder="Soles" disabled
+          <input className=' px-3 py-1 text-shadow1 text-2xl bg-transparent font-semibold w-24 text-yellow-400' placeholder="PEN" disabled
             onChange={(e) => setTitlesol(e.target.value)} value={titlesol} autoFocus />
           <span className='text-shadow1 text-yellow-400 font-semibold text-2xl  ms-64 mt-1  absolute' >Bs</span>
 
@@ -61,7 +61,7 @@ export function Prices() {
 
           <button className='  text-green-900 text-2xl font-bold px-2 py-1' disabled={true} >1,000 COP</button>
           <p className='text-3xl text-gray-100 text-shadow1'>=</p>
-          <input className='px-3 py-1 text-shadow1 font-semibold text-2xl text-yellow-400 bg-transparent w-24' placeholder="Pesos" disabled
+          <input className='px-3 py-1 text-shadow1 font-semibold text-2xl text-yellow-400 bg-transparent w-24' placeholder="COP" disabled
             onChange={(e) => setTitlepesos(e.target.value)} value={titlepesos} autoFocus />
           <span className='text-shadow1 text-yellow-400 font-semibold text-2xl  ms-64 mt-1  absolute' >Bs</span>
         </ul>
