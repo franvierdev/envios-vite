5e601f51af20f2a71cd00eadd3a677a05e7c8732
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2023-06-21 08:59:29 -0400

    padding

--- src/foot.jsx
+++ src/foot.jsx
@@ -1,6 +1,6 @@
 import whatsapp from '../imgs/whatsapp.png'
 export function Pie() {
-  return <div className=" flex justify-center bg-gradient-to-t from-green-700 via-green-700/70 to-transparent text-center py-2 mt-8 ">
+  return <div className=" flex justify-center bg-gradient-to-t from-green-700 via-green-700/70 to-transparent text-center mt-8 py-8 ">
     <img src={whatsapp} className="bg-green-600/60 rounded-full w-8" alt="..."></img>
     <h2 className="text-xl font-semibold text-gray-100 text-shadow">
       {/* +57 322 2813632 */}
