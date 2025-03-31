0f6f9060795090588556a56d90ee03691ac13416
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2025-03-30 23:33:56 -0400

    asd

--- src/Prices.jsx
+++ src/Prices.jsx
@@ -26,7 +26,7 @@ export function Prices({ title, setTitle, Sol, setSol, Pesos, setPesos }) {
   }
 
   return (
-    <div className='col-start-1 row-start-3 sm:justify-self-end lg:mt-10'>
+    <div className='col-start-1 row-start-3 lg:justify-self-end lg:mt-10'>
       <form onSubmit={handleSubmit} className="grid gap-3 text-center mt-6">
 
 
@@ -44,8 +44,8 @@ export function Prices({ title, setTitle, Sol, setSol, Pesos, setPesos }) {
         <div className='flex rounded-md mx-auto justify-between bg-gradient-to-r  from-gray-100/90 to-green-700/90 w-72'>
 
           <button className=' text-green-900 text-2xl font-bold px-2 py-1 ' disabled={true} >1 Sol</button>
-          <p className='text-3xl text-gray-100 text-shadow1'>=</p>
-          <input className=' px-3 py-1 text-shadow1 text-2xl bg-transparent font-semibold w-24 text-yellow-400' placeholder="PEN" disabled
+          <p className='text-3xl text-gray-100 text-shadow1 ms-5'>=</p>
+          <input className='  py-1 me-2 text-shadow1 text-2xl bg-transparent font-semibold w-24 text-yellow-400' placeholder="PEN" disabled
             onChange={(e) => setSol(e.target.value)} value={Sol} autoFocus />
           <span className='text-shadow1 text-yellow-400 font-semibold text-xl mt-1 me-3 -ms-20 ' >Bs</span>
 
@@ -56,7 +56,7 @@ export function Prices({ title, setTitle, Sol, setSol, Pesos, setPesos }) {
 
           <button className='  text-green-900 text-2xl font-bold px-2 py-1' disabled={true} >1,000 COP</button>
           <p className='text-3xl text-gray-100 text-shadow1'>=</p>
-          <input className='px-3 py-1 text-shadow1 font-semibold text-2xl text-yellow-400 bg-transparent w-24' placeholder="COP" disabled
+          <input className=' py-1 text-shadow1 font-semibold text-2xl text-yellow-400 bg-transparent w-24' placeholder="COP" disabled
             onChange={(e) => setPesos(e.target.value)} value={Pesos} autoFocus />
           <span className='text-shadow1 text-yellow-400 font-semibold text-xl mt-1 me-3 -ms-12' >Bs</span>
         </div>
