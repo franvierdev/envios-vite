04cc2155ea0a7a9b275cd131877867c9db9788b5
franvierdev <118144537+franvierdev@users.noreply.github.com>
2025-10-23 15:29:09 -0400

    Actualizar Prices.jsx

--- src/Prices.jsx
+++ src/Prices.jsx
@@ -8,7 +8,7 @@ export function Prices({ title, setTitle, Sol, setSol, Pesos, setPesos }) {
 
   function Calcular() {
     const dolar = +title - (+title * 0.1)
-    const dolar7 = +title - (+title * 0.06)
+    const dolar7 = +title - (+title * 0.05)
     const soles = (+dolar7 * 0.28)
     const pesos = (+title - (+title * 0.06)) * 0.25
 
