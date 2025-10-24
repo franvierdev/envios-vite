941dd2805e23913305b408b3770d57953161b33b
franvierdev <118144537+franvierdev@users.noreply.github.com>
2025-10-24 10:31:15 -0400

    Actualizar foot.jsx

--- src/foot.jsx
+++ src/foot.jsx
@@ -1,12 +1,12 @@
 import whatsapp from '../imgs/whatsapp.png'
 export function Pata() {
   return <div className="col-span-full flex justify-center bg-gradient-to-t from-green-700 via-green-700/70 to-transparent text-center mt-9 py-12 ">
-   {/* <img src={whatsapp} className="bg-green-600/60 rounded-full w-8" alt="..."></img>
+   <img src={whatsapp} className="bg-green-600/60 rounded-full w-8" alt="..."></img>
     <h2 className="text-2xl font-semibold text-gray-100 text-shadow">
-       +57 322 2813632 
-      +58 412 156 55 59
-      /* +58 424 512 87 19
-    </h2> */}
+       
+      +51 971 755 780
+      
+    </h2> 
   </div >
 
 }
